c0e515bb530367f7001d32b4f110630e39ed153f
BenHbrt <benherbert1983@googlemail.com>
2022-12-08 20:07:50 +0100

    Styling settings

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.scss'
 
 function App() {
   return (
-    <div className="dreidel">
-      Dreidel
+    <div className="container">
+      <div className="container_title">Dreidel</div>
+      <div className="container_dreidel"></div>
+      <div className="container_players">
+        <div className="container_players_player">Fred</div>
+        <div className="container_players_player">Fred</div>
+        <div className="container_players_player">Fred</div>
+        <div className="container_players_player">Fred</div>
+        <div className="container_players_player">Fred</div>
+      </div>
     </div>
   );
 }
