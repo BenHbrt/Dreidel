e8f728ef2a3eb3d24a1090acddf42c47d430e348
BenHbrt <benherbert1983@googlemail.com>
2022-12-19 15:52:00 +0100

    Styling of player entry

--- src/Components/PlayersSetup.js
+++ src/Components/PlayersSetup.js
@@ -36,24 +36,28 @@ const PlayersSetup = ({ setSettings, setPlayers, refreshPlayers, dreidelType, se
     }
 
     const addPlayer = () => {
-        const updated = newPlayers.map((player) => {
-            return player
-        })
-        updated.push(newName)
-        setNewPlayers(updated)
-        setNewName("")
+        if (newPlayers.length < 6) {
+            const updated = newPlayers.map((player) => {
+                return player
+            })
+            updated.push(newName)
+            setNewPlayers(updated)
+            setNewName("")
+        }
     }
 
     const startGame = () => {
-        newPlayers.forEach(element => {
-            final.push({
-                name: element,
-                geld: startGeld -1,
-                playing: true
-            })
-        });
-        setPlayers(final)
-        setSettings(false)
+        if (newPlayers.length > 1) {
+            newPlayers.forEach(element => {
+                final.push({
+                    name: element,
+                    geld: startGeld -1,
+                    playing: true
+                })
+            });
+            setPlayers(final)
+            setSettings(false)
+        }
     }
 
     const deleteItem = (index) => {
@@ -68,20 +72,25 @@ const PlayersSetup = ({ setSettings, setPlayers, refreshPlayers, dreidelType, se
 
     return (
         <div className="playerssetup">
-            <div className='playerssetup_style'>Dreidel Style: <span onClick={() => {setDreidelType(false)}} className={`playerssetup_style_optionD${dreidelType === false ? " active" : ""}`}>Diaspora</span><span onClick={() => {setDreidelType(true)}} className={`playerssetup_style_optionI${dreidelType === true ? " active" : ""}`}>Israeli</span></div>
+            <div className='playerssetup_style'>Dreidel Style:<span onClick={() => {setDreidelType(false)}} className={`playerssetup_style_optionD${dreidelType === false ? " active" : ""}`}>Diaspora</span><span onClick={() => {setDreidelType(true)}} className={`playerssetup_style_optionI${dreidelType === true ? " active" : ""}`}>Israeli</span></div>
             <div className='playerssetup_startgeld'>
-                <svg onClick={() => {geldChange("minus")}} className={`playerssetup_startgeld_button${startGeld <= 5 ? "-unactivated" : ""}`} xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M12 15.667 6.333 10 12 4.333l1.729 1.729L9.792 10l3.937 3.938Z"/></svg>
+                Starting Geld:
+                <div className='playerssetup_startgeld_buttons'>
+                <svg onClick={() => {geldChange("minus")}} className={`playerssetup_startgeld_buttons_button${startGeld <= 5 ? "-unactivated" : ""}`} xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M12 15.667 6.333 10 12 4.333l1.729 1.729L9.792 10l3.937 3.938Z"/></svg>
                 <span>{startGeld}</span>
-                <svg onClick={() => {geldChange("plus")}} className={`playerssetup_startgeld_button${startGeld >= 20 ? "-unactivated" : ""}`} xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="m8 15.667-1.729-1.729L10.208 10 6.271 6.062 8 4.333 13.667 10Z"/></svg>
+                <svg onClick={() => {geldChange("plus")}} className={`playerssetup_startgeld_buttons_button${startGeld >= 20 ? "-unactivated" : ""}`} xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="m8 15.667-1.729-1.729L10.208 10 6.271 6.062 8 4.333 13.667 10Z"/></svg>
+                </div>
             </div>
-            <div>Players</div>
+            <div className='playerssetup_players'>
+            <div>Enter Players</div>
+            <div className='playerssetup_players_input'>
             <input type="text" name="newPlayer" value={newName} onChange={inputName}></input><span className="playerssetup_addbutton" onClick={addPlayer}>+ Add Player</span>
+            </div>
             {newPlayers.length > 0 && newPlayers.map((player, i) => {
                 return <div className="playerssetup_player_item">{i + 1}: {player} <div onClick={() => deleteItem(i)}><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M6.75 16.583q-.542 0-.938-.395-.395-.396-.395-.938V5.5h-1V4.417H8v-.896h4v.896h3.583V5.5h-1v9.729q0 .583-.385.969-.386.385-.948.385ZM13.5 5.5h-7v9.75q0 .104.073.177t.177.073h6.5q.083 0 .167-.083.083-.084.083-.167ZM8.333 14h1.084V7H8.333Zm2.25 0h1.084V7h-1.084ZM6.5 5.5v10-.25Z"/></svg></div></div>
             })}
-            <br/>
-            <button onClick={startGame}>Start Game</button>
-            <button onClick={() => console.log(newPlayers)}>Pl</button>
+            <div className="playerssetup_startgame" onClick={startGame}><span>Start Game</span></div>
+            </div>
         </div>
     )
 }
