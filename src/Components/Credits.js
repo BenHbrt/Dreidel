eb4b4ad509e789bab760dc444a493c1d3498b542
BenHbrt <benherbert1983@googlemail.com>
2022-12-21 13:15:36 +0100

    Readme

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -5,7 +5,7 @@ const Credits = ({ setInfo }) => {
         <div className="credits">
             <div className="credits_title">Credits</div>
             <p>I wrote this app as part of my coding portfolio, which can be found at <a href="https://github.com/BenHbrt">https://github.com/BenHbrt</a>.</p>
-            <p>The inspiration for this project came simply from the time of year. I wanted to code something festive and seasonal, and as I like cooking latkes, I knew that Hanukkah was approaching!</p>
+            <p>The inspiration for this project came simply from the time of year. I wanted to code something festive and seasonal, and as I like cooking latkes, I knew that Hanukkah was approaching, and so decided to code a dreidel game!</p>
             <p>I would like to thank flaticon.com (link), as I used one of their free icons for the dreidel favicon for this project. The icon and a link to where it can be found can both be found below.</p>
             <div className='credits_credits'>
                 <img src={require('../Images/hebrew.png')} alt="Dreidel icon"></img>
